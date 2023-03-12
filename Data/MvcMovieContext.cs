5cf062be357db824a8aee6493f0a1e5f042b8176
TrickSoLogic <vishalbhat.com@gmail.com>
2023-03-12 22:09:58 +0530

    Add project files.

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlackCoderMovieApp.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlackCoderMovieApp.Models.Movie> Movie { get; set; }
+    }
+}
